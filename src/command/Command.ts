2e5d3235b87d9817bc4b82285a1354065fbbbdb8
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-06-04 23:11:56 +0300

    Added argument support for commands

--- src/command/Command.ts
+++ src/command/Command.ts
@@ -1,23 +1,56 @@
 export interface CommandOptions {
     readonly name:         string
     readonly action:       CommandAction
+    readonly minArgCount?: number
+    readonly maxArgCount?: number
     readonly description?: string | null
 }
 
-export type CommandAction = (ctx: VkBotContext) => void
+export type CommandAction = (ctx: VkBotContext, args: string[]) => void
 
 export default class Command {
     readonly name:        string
     readonly action:      CommandAction
+    readonly minArgCount: number
+    readonly maxArgCount: number
     readonly description: string | null
 
     constructor(options: CommandOptions) {
-        this.name        = options.name
-        this.action      = options.action
-        this.description = options.description ?? null
+        const name        = options.name
+        const action      = options.action
+        const minArgCount = options.minArgCount ?? 0
+        const maxArgCount = options.maxArgCount ?? minArgCount
+        const description = options.description ?? null
+
+        if (minArgCount > maxArgCount)
+            throw new Error("minArgCount is bigger than maxArgCount")
+
+        this.name        = name
+        this.action      = action
+        this.minArgCount = minArgCount
+        this.maxArgCount = maxArgCount
+        this.description = description
     }
 
     register(bot: VkBot) {
-        bot.command(`/${this.name}`, ctx => this.action(ctx))
+        bot.command(`/${this.name}`, (ctx, next) => {
+            const { text } = ctx.message
+
+            if (text == null) {
+                next?.()
+                return
+            }
+
+            const args = text.trim()
+                             .split(/\s+/)
+                             .slice(1)
+
+            if (args.length < this.minArgCount || args.length > this.maxArgCount) {
+                next?.()
+                return
+            }
+
+            this.action(ctx, args)
+        })
     }
 }
